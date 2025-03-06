effa882be3bd4b9545c1c620c24e7c0bbd6778a9
SakuraFox <982001940@qq.com>
2025-03-06 16:03:40 +0800

    style:注释

--- src/Hot100/Solution28.java
+++ src/Hot100/Solution28.java
@@ -21,83 +21,83 @@ public class Solution28 {
             value = v;
         }
     }
+//
+//    private final int capacity;
+//    private final Node dummy = new Node(0, 0);
+//    private final Map<Integer, Node> keyToNode = new HashMap<>();
 
-    private final int capacity;
-    private final Node dummy = new Node(0, 0);
-    private final Map<Integer, Node> keyToNode = new HashMap<>();
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        dummy.prev = dummy;
-        dummy.next = dummy;
-    }
-
-    public int get(int key) {
-        Node node = getNode(key);
-        return node != null ? node.value : -1;
-    }
-
-    public void put(int key, int value) {
-        Node node = getNode(key);
-        if (node != null) {
-            node.value = value;
-            return;
-        }
-        node = new Node(key, value);
-        keyToNode.put(key,node);
-        pushFront(node); // 放在最上面
-        if (keyToNode.size()>capacity){
-            Node backNode = dummy.prev;
-            keyToNode.remove(backNode.key);
-            remove(backNode);
-        }
-    }
-    private Node getNode(int key) {
-        if (!keyToNode.containsKey(key)) return null;
-        Node node = keyToNode.get(key);
-        remove(node);
-        pushFront(node);
-        return node;
-    }
-
-    private void remove(Node x) {
-        x.prev.next = x.next;
-        x.next.prev = x.prev;
-    }
-
-    private void pushFront(Node x) {
-        x.prev = dummy;
-        x.next = dummy.next;
-        x.prev.next = x;
-        x.next.prev = x;
-    }
+//    public LRUCache(int capacity) {
+//        this.capacity = capacity;
+//        dummy.prev = dummy;
+//        dummy.next = dummy;
+//    }
+//
+//    public int get(int key) {
+//        Node node = getNode(key);
+//        return node != null ? node.value : -1;
+//    }
+//
+//    public void put(int key, int value) {
+//        Node node = getNode(key);
+//        if (node != null) {
+//            node.value = value;
+//            return;
+//        }
+//        node = new Node(key, value);
+//        keyToNode.put(key,node);
+//        pushFront(node); // 放在最上面
+//        if (keyToNode.size()>capacity){
+//            Node backNode = dummy.prev;
+//            keyToNode.remove(backNode.key);
+//            remove(backNode);
+//        }
+//    }
+//    private Node getNode(int key) {
+//        if (!keyToNode.containsKey(key)) return null;
+//        Node node = keyToNode.get(key);
+//        remove(node);
+//        pushFront(node);
+//        return node;
+//    }
+//
+//    private void remove(Node x) {
+//        x.prev.next = x.next;
+//        x.next.prev = x.prev;
+//    }
+//
+//    private void pushFront(Node x) {
+//        x.prev = dummy;
+//        x.next = dummy.next;
+//        x.prev.next = x;
+//        x.next.prev = x;
+//    }
 //    ---------------------------------------------------------
-    private final int capacity;
-    private final Map<Integer, Integer> cache = new LinkedHashMap<>(); // 自带双向链表
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-    }
-    public int get(int key) {
-        Integer value = cache.remove(key);
-        if (value!=null){
-            cache.put(key,value);
-            return value;
-        }
-        return -1;
-    }
-
-    public void put(int key, int value) {
-        if (cache.remove(key)!=null){
-            cache.put(key,value);
-            return;
-        }
-        if (cache.size()==capacity){
-            Integer olderKey = cache.keySet().iterator().next();
-            cache.remove(olderKey);
-        }
-        cache.put(key,value);
-
-    }
+//    private final int capacity;
+//    private final Map<Integer, Integer> cache = new LinkedHashMap<>(); // 自带双向链表
+//    public LRUCache(int capacity) {
+//        this.capacity = capacity;
+//    }
+//    public int get(int key) {
+//        Integer value = cache.remove(key);
+//        if (value!=null){
+//            cache.put(key,value);
+//            return value;
+//        }
+//        return -1;
+//    }
+//
+//    public void put(int key, int value) {
+//        if (cache.remove(key)!=null){
+//            cache.put(key,value);
+//            return;
+//        }
+//        if (cache.size()==capacity){
+//            Integer olderKey = cache.keySet().iterator().next();
+//            cache.remove(olderKey);
+//        }
+//        cache.put(key,value);
+//
+//    }
 
 
 }
